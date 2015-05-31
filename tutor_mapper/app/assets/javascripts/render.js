76419608ad7a40b1d617ddcb3be7bb966ef149f4
William Wu <williamwu.2k12@gmail.com>
2015-05-31 03:21:17 -0700

    changing lat/lng

--- tutor_mapper/app/assets/javascripts/render.js
+++ tutor_mapper/app/assets/javascripts/render.js
@@ -3,20 +3,34 @@
 
 window.onload = function(){
     handler = Gmaps.build('Google');
-    handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
-        markers = handler.addMarkers([
+    handler.buildMap(
+        { 
+            provider: 
             {
-                "lat": 0,
-                "lng": 0,
-                "picture": {
-                    "url": "https://addons.cdn.mozilla.net/img/uploads/addon_icons/13/13028-64.png",
-                    "width":    36,
-                    "height": 36
-                },
-                "infowindow": "hello!"
+                disableDefaultUI: true
+                // pass in other Google Maps API options here
+            }, 
+            internal: 
+            {
+                id: 'map'
             }
-        ]);
-        handler.bounds.extendWith(markers);
-        handler.fitMapToBounds();
-    });
+        }, 
+        function()
+        {
+            markers = handler.addMarkers([
+                {
+                    "lat": 37.864677,
+                    "lng": -122.256738,
+                    // "picture": {
+                    //     "url": "https://addons.cdn.mozilla.net/img/uploads/addon_icons/13/13028-64.png",
+                    //     "width": 36,
+                    //     "height": 36
+                    // },
+                    "infowindow": "hello!"
+                }
+            ]);
+            handler.bounds.extendWith(markers);
+            handler.fitMapToBounds();
+        }
+    );
 }
